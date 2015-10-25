3feb4d62ea094809e017b095c1a26699d3b51c75
giraus <cool.giraus@yandex.ru>
2015-10-25 16:56:52 +0300

    Update Program.cs

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -14,30 +14,16 @@ namespace WebCrawler
     {
         public static void Main(string[] args)
         {
-            //LinkStorage storage = new LinkStorage();
-            //storage.GetInitialLink();
-            //GraphMaker maker = new GraphMaker();
-            //Node node = null;
-            //while (storage.IsNeedVisit())
-            //{
-            //    node = storage.Visit();
-            //    maker.Make(node);
-            //}
+            LinkStorage storage = new LinkStorage();
+            storage.GetInitialLink();
+            GraphMaker maker = new GraphMaker();
+            Node node = null;
+            while (storage.IsNeedVisit())
+            {
+                node = storage.Visit();
+                maker.Make(node);
+            }
 
-
-            FictiveParser parser = new FictiveParser();
-            parser.TagStart = "<a";
-            parser.TagTwoStart = "href=\"";
-            parser.TagEnd = "\"";
-
-            const string FileName = "File.html";
-
-            //Объявляем очередь5
-            List<Link> links = new List<Link>();
-
-            links = parser.Parse(FileName);
-
-            Console.ReadLine();
            
         }
     }
