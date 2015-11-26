1a784fea4132665bb8fc14e15b8e5a402e5aa25a
sawafrolov <sawa.frolov@yandex.ru>
2015-11-26 13:03:54 +0300

    task 13 check exception in downloader

--- WebCrawler/Downloader/Downloader.cs
+++ WebCrawler/Downloader/Downloader.cs
@@ -16,23 +16,8 @@ namespace WebCrawler.Downloader
         /// <returns>String with HTML code of page</returns>
         public String Load(string url)
         {
-            return getHTML(url);
-        }
-
-        protected string getHTML(string url)
-        {
-            //TODO: 404 error exception
-            HttpWebRequest request;
-            HttpWebResponse response;
-            try
-            {
-                  request = WebRequest.Create(url) as HttpWebRequest;
-                  response = request.GetResponse() as HttpWebResponse;
-            } catch (System.Net.WebException e)
-            {
-                return "404: " + url;
-            }
-
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest; ;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
             string html = "";
             using (StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
             {
---
+++ WebCrawler/Graph/ProblemLink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebCrawler.Graph
+{
+    /// <summary>
+    /// Ссылка на проблемную страницу
+    /// </summary>
+    class ProblemLink : Link
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="link">URL ссылки</param>
+        public ProblemLink(String link) : base(link) {}
+    }
+}
